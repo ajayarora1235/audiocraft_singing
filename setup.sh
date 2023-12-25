72e60cd962b2866fe8155ce2a0619638ef197898
Ajay Arora <ajayarora1235@gmail.com>
2023-12-25 00:51:39 +0000

    finalize setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,8 @@ sudo apt install libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0 f
 pip install PyAudio BeatNet
 wandb init
 sudo apt-get install zip gzip tar libsox-dev
-conda install -c conda-forge sox
 cd dataset/songsinger
 python3 -m new_data_getter
 unzip full_dataset.zip -d .
-python3 -m directory_renamer
 cd ../..
-python3  dataset/songsinger/data_checker.py
+python3 dataset/songsinger/data_checker.py
