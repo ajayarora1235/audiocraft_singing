a59b103546999330865ddd388fdafc9cae34fe2b
Ajay Arora <ajayarora1235@gmail.com>
2023-12-31 16:28:36 -0500

    make final changes to branch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ pip install boto3 wandb pydub
 sudo apt-get update
 yes Y | sudo apt-get install zip gzip tar ffmpeg libsox-dev
 cd dataset/songsinger
-python3 -m new_data_getter
+python3 -m data_getter
 unzip full_dataset.zip -d .
 cd ../..
